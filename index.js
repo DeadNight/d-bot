3ba347c0d23aea267189fdea6b7d58446c91f9a7
Nir Leibovitch <nir.leibo@gmail.com>
2020-02-01 10:19:35 +0200

    fix

--- index.js
+++ index.js
@@ -284,7 +284,7 @@ function getMemberData(member) {
 }
 
 function getHostData(account, member) {
-  return new Promise(resolve, reject) => {
+  return new Promise((resolve, reject) => {
     getMemberData(member).then((memberData) => {
       return memberData.hosts.get(account);
     }).reject((err) => {
