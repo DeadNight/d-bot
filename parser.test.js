b612d7ba7e7a226eecc56459029217a3c8b7cc83
Nir Leibovitch <nir.leibo@gmail.com>
2020-02-19 21:02:55 +0200

    Update parser.test.js

--- parser.test.js
+++ parser.test.js
@@ -58,7 +58,7 @@ add the 3ds friend code, IGN: Britany
   });
 });
 
-function cleanObject (obj) {
+function cleanObject(obj) {
   Object.keys(obj).forEach(key => {
     if(obj[key] === 'undefined')
       delete obj[key]
