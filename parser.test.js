30950a0351944b8fddbeb5818826e2525b4195d5
Nir Leibovitch <nir.leibo@gmail.com>
2020-02-19 21:26:30 +0200

    add empty cmd parser test

--- parser.test.js
+++ parser.test.js
@@ -1,73 +1,34 @@
 const { parseCommand, parseFlags } = require('./parser.js');
 
-describe('regression', () => {
-  describe('parseCommand', () => {
-    test('test set -a -d', () => {
-    let cmd = `!h test set corny rapidash -a 3ds -d \`\`\`asciidoc
+describe('parseCommand', () => {
+  test('empty cmd', () => {
+    let cmd = '!h';
+
+    let expected = [
+      {
+        prefix: '!h',
+      }
+    ];
+
+    let actual = cleanObject(parseCommand(cmd));
+
+    expect(actual).toEqual(expected);
+  });
+  
+  test('test set -a -d', () => {
+  let cmd = `!h test set corny rapidash -a 3ds -d \`\`\`asciidoc
 rapidash
 add the 3ds friend code, IGN: Britany
 \`\`\``;
 
-      let expected = [
-        {
-          prefix: '!h',
-          test: 'test',
-          cmd: 'set',
-          _: 'corny rapidash',
-        },
-        [
-          {
-            flagName: '-a',
-            flagValue: '3ds'
-          },
-          {
-            flagName: '-d',
-            flagValue: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
-          }
-        ]
-      ];
-      
-      let actual = cleanObject(parseCommand(cmd));
-
-      expect(actual).toEqual(expected);
-    });
-    
-    test('set -a -d', () => {
-      let cmd = `!host set Shiny Litwick -a 3ds -d \`\`\`diff
-- 🌟Litwick
---- add 3ds friend code, name Levi
-\`\`\``;
-      
-      let expected = [
-        {
-          prefix: '!host',
-          cmd: 'set',
-          _: 'Shiny Litwick',
-        },
-        [
-          {
-            flagName: '-a',
-            flagValue: '3ds'
-          },
-          {
-            flagName: '-d',
-            flagValue: `\`\`\`diff
-- 🌟Litwick
---- add 3ds friend code, name Levi
-\`\`\``
-          }
-        ]
-      ];
-      
-      let actual = cleanObject(parseCommand(cmd));
-
-      expect(actual).toEqual(expected);
-    });
-  });
-
-  describe('parseFlags', () => {
-    test('set -a -d', () => {
-      let flags = [
+    let expected = [
+      {
+        prefix: '!h',
+        test: 'test',
+        cmd: 'set',
+        _: 'corny rapidash',
+      },
+      [
         {
           flagName: '-a',
           flagValue: '3ds'
@@ -76,24 +37,75 @@ add the 3ds friend code, IGN: Britany
           flagName: '-d',
           flagValue: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
         }
-      ];
+      ]
+    ];
 
-      let expected = {
-        account: '3ds',
-        description: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
-      };
-      
-      let actual = cleanObject(parseFlags(flags, {
-        '-a': 'account',
-        '--account': 'account',
-        '-c': 'code',
-        '--code': 'code',
-        '-d': 'description',
-        '--description': 'description'
-      }));
+    let actual = cleanObject(parseCommand(cmd));
 
-      expect(actual).toEqual(expected);
-    });
+    expect(actual).toEqual(expected);
+  });
+
+  test('set -a -d', () => {
+    let cmd = `!host set Shiny Litwick -a 3ds -d \`\`\`diff
+- 🌟Litwick
+--- add 3ds friend code, name Levi
+\`\`\``;
+
+    let expected = [
+      {
+        prefix: '!host',
+        cmd: 'set',
+        _: 'Shiny Litwick',
+      },
+      [
+        {
+          flagName: '-a',
+          flagValue: '3ds'
+        },
+        {
+          flagName: '-d',
+          flagValue: `\`\`\`diff
+- 🌟Litwick
+--- add 3ds friend code, name Levi
+\`\`\``
+        }
+      ]
+    ];
+
+    let actual = cleanObject(parseCommand(cmd));
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('parseFlags', () => {
+  test('set -a -d', () => {
+    let flags = [
+      {
+        flagName: '-a',
+        flagValue: '3ds'
+      },
+      {
+        flagName: '-d',
+        flagValue: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
+      }
+    ];
+
+    let expected = {
+      account: '3ds',
+      description: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
+    };
+
+    let actual = cleanObject(parseFlags(flags, {
+      '-a': 'account',
+      '--account': 'account',
+      '-c': 'code',
+      '--code': 'code',
+      '-d': 'description',
+      '--description': 'description'
+    }));
+
+    expect(actual).toEqual(expected);
   });
 });
 
