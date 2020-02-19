41a60b7bd52f32b2a84e9f1769d0877117778793
Nir Leibovitch <nir.leibo@gmail.com>
2020-02-19 20:59:29 +0200

    fix test

--- parser.test.js
+++ parser.test.js
@@ -1,20 +1,41 @@
 const { parseCommand, parseFlags } = require('./parser.js');
 
-describe('parseCommand', () => {
-  test('should parse correctly', () => {
-  let cmd = `!h test set corny rapidash -a 3DS -d \`\`\`asciidoc
-rapidash
-add the 3ds friend code, IGN: Britany
-\`\`\``;
-    
-    let expected = [
-      {
-        prefix: '!h',
-        test: 'test',
-        cmd: 'set',
-        _: 'corny rapidash',
-      },
-      [
+describe('regression', () => {
+  describe('parseCommand', () => {
+    test('should parse correctly', () => {
+    let cmd = `!h test set corny rapidash -a 3ds -d \`\`\`asciidoc
+  rapidash
+  add the 3ds friend code, IGN: Britany
+  \`\`\``;
+
+      let expected = [
+        {
+          prefix: '!h',
+          test: 'test',
+          cmd: 'set',
+          _: 'corny rapidash',
+        },
+        [
+          {
+            flagName: '-a',
+            flagValue: '3ds'
+          },
+          {
+            flagName: '-d',
+            flagValue: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
+          }
+        ]
+      ];
+      
+      let actual = cleanObject(parseCommand(cmd));
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('parseFlags', () => {
+    test('should parse correctly', () => {
+      let flags = [
         {
           flagName: '-a',
           flagValue: '3ds'
@@ -23,31 +44,24 @@ add the 3ds friend code, IGN: Britany
           flagName: '-d',
           flagValue: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
         }
-      ]
-    ];
-    
-    expect(parseCommand(cmd)).toEqual(expected);
+      ];
+
+      let expected = {
+        account: '3ds',
+        description: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
+      };
+      
+      let actual = cleanObject(parseFlags(flags));
+
+      expect(actual).toEqual(expected);
+    });
   });
 });
 
-describe('parseFlags', () => {
-  test('should parse correctly', () => {
-    let flags = [
-      {
-        flagName: '-a',
-        flagValue: '3ds'
-      },
-      {
-        flagName: '-d',
-        flagValue: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
-      }
-    ];
-
-    let expected = {
-      account: '3ds',
-      description: '```asciidoc\nrapidash\nadd the 3ds friend code, IGN: Britany\n```'
-    };
-
-    expect(parseFlags(flags)).toEqual(expected);
+function cleanObject (obj) {
+  Object.keys(obj).forEach(key => {
+    if(obj[key] === 'undefined')
+      delete obj[key]
   });
-});
+  return obj;
+}
